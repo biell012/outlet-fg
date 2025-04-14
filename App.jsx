b541294f7dfa8eb77bfbc491904b2b0e569767ce
biell012 <biellsilva0080@gmail.com>
2025-04-14 16:22:23 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-2xl font-bold text-blue-600">
+      Bem-vindo à Outlet-FG
+    </div>
+  );
+}
+
+export default App;
